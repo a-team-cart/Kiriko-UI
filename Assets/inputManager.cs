6878e292ab76457294215b1a588f0971b0a0a7fc
Charles Doucet <charles.douc@gmail.com>
2018-11-11 22:18:50 -0500

    Integrated Midi Controller for inputs. Linked it to the UI.

---
+++ Assets/inputManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class inputManager : MonoBehaviour {
+
+	// public variables --------------------
+	public float m_saturation;									// Saturation of colors in the scene
+	public float m_gravity;											// Gravity scale in the scene
+	public float m_timeSpeed;										// Time speed for motion in the scene
+	public bool m_lightState;										// Lights on or off
+	public float m_lightIntensity;							// Lights intensity in the scene
+	public float m_soundPitch;									// Global sound pitch of a soudscape
+	public int[] m_soundScapeIndex;							// Soundscape index 
+	public float m_normalIntensity;							// Normal intensity of textures in the scene
+	public int[] m_spawnObjectIndex;						// Number of objects spawn in the scene
+	public float m_noiseIntensity;							// Noise intensity for the volumetric lighting
+	public float m_chromaticAberration;					// Intensity of cromatic aberratin (PPS)
+	public float m_stormState;									// Intensity of the weather storm (rain - snow)
+
+	// private variables -------------------
+	private float dataTest = 0f;
+	
+	// -------------------------------------
+	// Use this for initialization
+	// -------------------------------------
+	void Start () {
+
+		
+		
+		
+	}
+	
+
+	// -------------------------------------
+	// Update is called once per frame
+	// -------------------------------------
+	void Update () {
+
+		// Listen for new inputs
+	    fetchData();
+	}
+
+
+	// -------------------------------------
+	// Methods
+	// -------------------------------------
+	// Fetching data from the inputs
+	private void fetchData() {
+		m_saturation = MidiMaster.GetKnob(41);
+		m_gravity = MidiMaster.GetKnob(42);
+		m_timeSpeed = MidiMaster.GetKnob(43);
+		m_lightIntensity = MidiMaster.GetKnob(44);
+		m_soundPitch = MidiMaster.GetKnob(45);
+		m_normalIntensity = MidiMaster.GetKnob(46);
+	}
+
+
+}
