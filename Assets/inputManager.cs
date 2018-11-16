8fe1829ec370673d16ec8bd2dea972fb92449203
Charles Doucet <charles.douc@gmail.com>
2018-11-16 15:54:24 -0500

    Integrated the new midi controller.

--- Assets/inputManager.cs
+++ Assets/inputManager.cs
@@ -7,16 +7,16 @@ public class inputManager : MonoBehaviour {
 
 	// public variables --------------------
 	public float m_saturation;									// Saturation of colors in the scene
-	public float m_gravity;											// Gravity scale in the scene
-	public float m_timeSpeed;										// Time speed for motion in the scene
-	public bool m_lightState;										// Lights on or off
-	public float m_lightIntensity;							// Lights intensity in the scene
+	public float m_gravity;										// Gravity scale in the scene
+	public float m_timeSpeed;									// Time speed for motion in the scene
+	public bool m_lightState;									// Lights on or off
+	public float m_lightIntensity;								// Lights intensity in the scene
 	public float m_soundPitch;									// Global sound pitch of a soudscape
-	public int[] m_soundScapeIndex;							// Soundscape index 
-	public float m_normalIntensity;							// Normal intensity of textures in the scene
-	public int[] m_spawnObjectIndex;						// Number of objects spawn in the scene
-	public float m_noiseIntensity;							// Noise intensity for the volumetric lighting
-	public float m_chromaticAberration;					// Intensity of cromatic aberratin (PPS)
+	public int[] m_soundScapeIndex;								// Soundscape index 
+	public float m_normalIntensity;								// Normal intensity of textures in the scene
+	public int[] m_spawnObjectIndex;							// Number of objects spawn in the scene
+	public float m_noiseIntensity;								// Noise intensity for the volumetric lighting
+	public float m_chromaticAberration;							// Intensity of cromatic aberratin (PPS)
 	public float m_stormState;									// Intensity of the weather storm (rain - snow)
 
 	// private variables -------------------
@@ -48,12 +48,12 @@ public class inputManager : MonoBehaviour {
 	// -------------------------------------
 	// Fetching data from the inputs
 	private void fetchData() {
-		m_saturation = MidiMaster.GetKnob(41);
-		m_gravity = MidiMaster.GetKnob(42);
-		m_timeSpeed = MidiMaster.GetKnob(43);
-		m_lightIntensity = MidiMaster.GetKnob(44);
-		m_soundPitch = MidiMaster.GetKnob(45);
-		m_normalIntensity = MidiMaster.GetKnob(46);
+		m_saturation = MidiMaster.GetKnob(1);
+		m_gravity = MidiMaster.GetKnob(2);
+		m_timeSpeed = MidiMaster.GetKnob(3);
+		m_lightIntensity = MidiMaster.GetKnob(4);
+		m_soundPitch = MidiMaster.GetKnob(5);
+		m_normalIntensity = MidiMaster.GetKnob(6);
 	}
 
 
